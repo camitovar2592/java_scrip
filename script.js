d0d7fdcda574e427f4e896656db37dbb66440c46
Camilo <camitovar2592@gmail.com>
2026-07-30 19:56:11 -0500

    promedios

--- script.js
+++ script.js
@@ -20,15 +20,25 @@
 // let pesoVenus = peso * 0.9;
 // console.log(`Hola ${nombre} tu peso en la luna es ${pesoLuna} Kg, tu peso en Marte es ${pesoMarte} Kg y tu peso en Venus es ${pesoVenus}Kg`)
 
-let altura = Number(prompt('Digite la altura del triangulo'));
-let base = Number(prompt('Digite la base del triangulo'));
-let areaTriangulo = altura * base / 2;
-console.log(`Hola el area del triangulo es ${areaTriangulo}`)
+// let altura = Number(prompt('Digite la altura del triangulo'));
+// let base = Number(prompt('Digite la base del triangulo'));
+// let areaTriangulo = altura * base / 2;
+// console.log(`Hola el area del triangulo es ${areaTriangulo}`)
 
-let centrigados = Number(prompt('Digite la temperatura en Centrigados'));
-let temperaturaFaren = ((centrigados*(9/5))+32);
-console.log(`Hola la temperatura en Fahrenheit es ${temperaturaFaren}`)
+// let centrigados = Number(prompt('Digite la temperatura en Centrigados'));
+// let temperaturaFaren = ((centrigados*(9/5))+32);
+// console.log(`Hola la temperatura en Fahrenheit es ${temperaturaFaren}`)
 
-let distanciaKm = Number(prompt('Digite la distancia en Kilometros'));
-let metros = (distanciaKm*1000);
-console.log(`Hola la distancia en metros es ${metros}`)
+// let distanciaKm = Number(prompt('Digite la distancia en Kilometros'));
+// let metros = (distanciaKm*1000);
+// console.log(`Hola la distancia en metros es ${metros}`)
+
+let precio = Number(prompt('Digite el precio del producto'));
+let descuento = precio * 0.8;
+console.log(`Hola el precio con descuento es ${descuento}`)
+
+let nota1 = Number(prompt('Digite la nota 1'));
+let nota2 = Number(prompt('Digite la nota 2'));
+let nota3 = Number(prompt('Digite la nota 3'));
+let promedio = (nota1 + nota2 + nota3) / 3;
+console.log(`Hola Alumno, la nota promedio es ${promedio}`)
