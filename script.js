929d2f28480080dd5268f71872465711c97abafb
Camilo <camitovar2592@gmail.com>
2026-07-30 19:48:53 -0500

    number

--- script.js
+++ script.js
@@ -20,15 +20,15 @@
 // let pesoVenus = peso * 0.9;
 // console.log(`Hola ${nombre} tu peso en la luna es ${pesoLuna} Kg, tu peso en Marte es ${pesoMarte} Kg y tu peso en Venus es ${pesoVenus}Kg`)
 
-let altura = prompt('Digite la altura del triangulo');
-let base = prompt('Digite la base del triangulo');
+let altura = Number(prompt('Digite la altura del triangulo'));
+let base = Number(prompt('Digite la base del triangulo'));
 let areaTriangulo = altura * base / 2;
 console.log(`Hola el area del triangulo es ${areaTriangulo}`)
 
-let centrigados = prompt('Digite la temperatura en Centrigados');
+let centrigados = Number(prompt('Digite la temperatura en Centrigados'));
 let temperaturaFaren = ((centrigados*(9/5))+32);
 console.log(`Hola la temperatura en Fahrenheit es ${temperaturaFaren}`)
 
-let distanciaKm = prompt('Digite la distancia en Kilometros');
+let distanciaKm = Number(prompt('Digite la distancia en Kilometros'));
 let metros = (distanciaKm*1000);
 console.log(`Hola la distancia en metros es ${metros}`)
