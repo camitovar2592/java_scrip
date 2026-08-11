fd502ccf60a78062818d02437b65ea3d0657a3f6
Camilo <camitovar2592@gmail.com>
2026-08-11 18:42:22 -0500

    ejercicio 2

--- taller.js
+++ taller.js
@@ -1,6 +1,6 @@
 //Ejercicio 1
 
-let colores = ["Rojo", "Verde", "Azul", "Amarillo", "Morado"];
+let colores = ["Amarillo", "Azul", "Rojo", "Verde", "Morado"];
 
 console.log("Primer color:", colores[0]);
 
@@ -8,3 +8,8 @@ console.log("Último color:", colores[colores.length - 1]);
 
 // Ejercicio 2
 
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 651351];
+
+console.log("El array tiene elementos:", numeros.length);
+
+//
