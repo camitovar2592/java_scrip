1312e8819d9dbe05cbaada001a79db2711c46cff
Camilo <camitovar2592@gmail.com>
2026-08-11 18:49:35 -0500

    ejercicio 3

--- taller.js
+++ taller.js
@@ -12,4 +12,10 @@ let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 651351];
 
 console.log("El array tiene elementos:", numeros.length);
 
+// Ejercicio 3
+
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
+}
+
 //
